e578138496de7d7976e04bf886d2a6b483c4c27d
seongwook03 <ppapkkanh@gmail.com>
2026-03-13 15:29:49 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args){
 
         System.out.println("Hello My First Java Coding!!");
+
+        System.out.println("Edite Java Code!!");
     }
 }
