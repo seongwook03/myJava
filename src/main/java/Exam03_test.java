835605d15aeb839e84e7d36bcb9c6c8148ad9414
seongwook03 <ppapkkanh@gmail.com>
2026-04-09 14:52:46 +0900

    문자열 뽑아내기

--- src/main/java/Exam03_test.java
+++ src/main/java/Exam03_test.java
@@ -1,12 +1,14 @@
 public class Exam03_test {
     public static void main(String[] args) {
 
-        String log = "127.0.0.1 - - [31/Mar/2026:10:00:01 +0900] \"GET /index.html HTTP/1.1\" 200 1043 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\"\n";
+        String log ="127.0.0.1 - - [31/Mar/2026:10:00:01 +0900] \"GET /index.html HTTP/1.1\" 200 1043 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\"";
         int space = log.indexOf(" ", log.indexOf("\""));
 
         System.out.println(space);
         String res = log.substring(space, log.indexOf(" ", space + 1));
 
         System.out.println("접속한 페이지 : " + res);
+
+
     }
 }
